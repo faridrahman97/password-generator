73c813e11c34d7e5beb363ca29d75371ac221b96
Farid <beatsbyfar.id@gmail.com>
2022-10-16 20:02:45 -0600

    Add files from local machine

---
+++ js/index.js
@@ -0,0 +1,35 @@
+let firstPass = document.getElementById("first-output");
+let secondPass = document.getElementById("second-output");
+let passwordLength = document.getElementById("password-length-field");
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+function generatePasswords() {
+    firstPass.textContent = getRandomPassword();
+    secondPass.textContent = getRandomPassword();
+}
+
+function getRandomPassword() {
+    let length = passwordLength.value;
+    if (length === ""){
+        length = 15;
+    } else {
+        length = parseInt(length);
+    }
+
+    let randomPassword = "";
+    for (let i = 0; i < length; i++) {
+        randomPassword += characters[Math.floor(Math.random() * characters.length)]
+    }
+
+    return randomPassword;
+}
+
+function copyToClipboard() {
+    var range = document.createRange();
+    range.selectNode(document.getElementById("first-output"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+}
